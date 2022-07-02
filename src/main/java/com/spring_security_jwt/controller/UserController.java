f952d4680b1b27b748b9034a31afced63168d02c
Felippe Negrão de Oliveira <felippenegrao@alunos.utfpr.edu.br>
2022-07-01 21:59:08 -0300

    project finished

---
+++ src/main/java/com/spring_security_jwt/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.spring_security_jwt.controller;
+
+import com.spring_security_jwt.model.User;
+import com.spring_security_jwt.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserService service;
+    @PostMapping
+    public void postUser(@RequestBody User user){
+        service.createUser(user);
+    }
+}
